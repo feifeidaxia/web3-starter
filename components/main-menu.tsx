e5fd6c7fe04ede616447b2cde7377c6ca2576502
feifeidaxia <1368260445@qq.com>
2024-03-18 09:12:43 +0800

    11

--- components/main-menu.tsx
+++ components/main-menu.tsx
@@ -13,7 +13,7 @@ const MainMenu = memo(function MainMenu() {
   return (
     <div className="h-lvh">
       {isConnected && <UesrMenu address={address} />}
-      <Spline scene="https://prod.spline.design/YMLwHBrZcb2CsruE/scene.splinecode" />
+      {/* <Spline scene="https://prod.spline.design/YMLwHBrZcb2CsruE/scene.splinecode" /> */}
       {pageLoading && (
         <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-gray-200 bg-opacity-50">
           <ReloadIcon className="h-4 w-4 animate-spin" />
